c08204774fb8fd45e09408cf78a0ece427dc2236
Daniel Vidmar <daniel.viddy@gmail.com>
2014-11-13 21:21:07 -0500

    Restructured to include a test directory and added .travis.yml for Travis CI

---
+++ test/setup.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Daniel Vidmar.
+ * Date: 11/13/14
+ * Time: 9:09 PM
+ * Version: Beta 2
+ * Last Modified: 11/13/14 at 9:09 PM
+ * Last Modified by Daniel Vidmar.
+ */
+
+//TODO: parse travis-ci-tables.sql and move travis-ci-config.ini to correct directory
+?>
