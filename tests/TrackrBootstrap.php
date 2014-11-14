ecb51af37986e7054d9de92b2632be48162c9ddf
Daniel Vidmar <daniel.viddy@gmail.com>
2014-11-14 13:30:54 -0500

    Added autoload functions

--- tests/TrackrBootstrap.php
+++ tests/TrackrBootstrap.php
@@ -7,7 +7,30 @@
  * Last Modified: 11/14/14 at 12:03 AM
  * Last Modified by Daniel Vidmar.
  */
-if (file_exists(__DIR__.'/../vendor/autoload.php')) {
-    require_once(__DIR__.'/../vendor/autoload.php');
+function api_loader($class) {
+    load("/src/api/".$class.".php");
 }
+
+function include_loader($class) {
+    load("/src/include/".$class.".php");
+}
+
+function class_loader($class) {
+    load("/src/include/class/".$class.".php");
+}
+
+function function_loader($class) {
+    load("/src/include/function/".$class.".php");
+}
+
+function load($file) {
+    if (file_exists($file)) {
+        require $file;
+    }
+}
+
+spl_autoload_register('api_loader');
+spl_autoload_register('include_loader');
+spl_autoload_register('class_loader');
+spl_autoload_register('function_loader');
 ?>
